bc38783237aab9e2337c1a31f8ba22ac90b3471e
Peiyuan Zhu <garyzhubc@Peiyuans-MacBook-Pro.local>
2020-04-19 18:24:33 -0700

    added capsnet to resnet

--- project/UnsupervisedGeometryAwareRepresentationLearning/python/models/resnet_VNECT_3Donly.py
+++ project/UnsupervisedGeometryAwareRepresentationLearning/python/models/resnet_VNECT_3Donly.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 #sys.path.insert(0,'../')
 
 from utils import training as utils_train
+from torch.autograd import Variable
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152']
@@ -98,6 +99,52 @@ class Bottleneck(nn.Module):
 
         return out
 
+def softmax(input, dim=1):
+    transposed_input = input.transpose(dim, len(input.size()) - 1)
+    softmaxed_output = F.softmax(transposed_input.contiguous().view(-1, transposed_input.size(-1)), dim=-1)
+    return softmaxed_output.view(*transposed_input.size()).transpose(dim, len(input.size()) - 1)
+
+class CapsuleLayer(nn.Module):
+    def __init__(self, num_capsules, num_route_nodes, in_channels, out_channels, kernel_size=None, stride=None,
+                 num_iterations=3):
+        super(CapsuleLayer, self).__init__()
+
+        self.num_route_nodes = num_route_nodes
+        self.num_iterations = num_iterations
+
+        self.num_capsules = num_capsules
+
+        if num_route_nodes != -1:
+            self.route_weights = nn.Parameter(torch.randn(num_capsules, num_route_nodes, in_channels, out_channels))
+        else:
+            self.capsules = nn.ModuleList(
+                [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=0) for _ in
+                 range(num_capsules)])
+
+    def squash(self, tensor, dim=-1):
+        squared_norm = (tensor ** 2).sum(dim=dim, keepdim=True)
+        scale = squared_norm / (1 + squared_norm)
+        return scale * tensor / torch.sqrt(squared_norm)
+
+    def forward(self, x):
+        if self.num_route_nodes != -1:
+            priors = x[None, :, :, None, :] @ self.route_weights[:, None, :, :, :]
+
+            logits = Variable(torch.zeros(*priors.size()))
+            for i in range(self.num_iterations):
+                probs = softmax(logits, dim=2)
+                outputs = self.squash((probs * priors).sum(dim=2, keepdim=True))
+
+                if i != self.num_iterations - 1:
+                    delta_logits = (priors * outputs).sum(dim=-1, keepdim=True)
+                    logits = logits + delta_logits
+        else:
+            outputs = [capsule(x).view(x.size(0), -1, 1) for capsule in self.capsules]
+            outputs = torch.cat(outputs, dim=-1)
+            outputs = self.squash(outputs)
+
+        return outputs
+
 
 class ResNetTwoStream(nn.Module):
 
@@ -133,7 +180,8 @@ class ResNetTwoStream(nn.Module):
         heat_vec_width     = int(input_width/32)-3
         heat_vec_dimension = 128*heat_vec_width*heat_vec_width
 
-        self.fc = nn.Linear(l4_vec_dimension, num_classes)
+        # self.fc = nn.Linear(l4_vec_dimension, num_classes)
+        self.fc = nn.Linear(160, num_classes)
         
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -142,6 +190,12 @@ class ResNetTwoStream(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
+
+        self.conv1a = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=13, stride=1)
+        self.primary_capsules = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
+                                             kernel_size=9, stride=2)
+        self.digit_capsules = CapsuleLayer(num_capsules=10, num_route_nodes=32 * 6 * 6, in_channels=8,
+                                           out_channels=16)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -162,23 +216,37 @@ class ResNetTwoStream(nn.Module):
 
     def forward(self, x_dict):
         x = x_dict[self.input_key]
-        
         x = self.conv1(x) # size /2
         x = self.bn1(x)
         x = self.relu(x) 
         x = self.maxpool(x) # size /2
 
-        x = self.layer1(x) 
-        x = self.layer2(x)# size /2
-        x = self.layer3(x)# size /2
-                
+        x = self.layer1(x)
+
+        x = F.relu(self.conv1a(x), inplace=True)
+        x = self.primary_capsules(x)
+        x = self.digit_capsules(x)
+
+        # x = self.layer2(x)# size /2
+        # x = self.layer3(x)# size /2
+
+        x = x.squeeze().transpose(0, 1)
+        classes = (x ** 2).sum(dim=-1) ** 0.5
+        classes = F.softmax(classes, dim=-1)
+
+        _, max_length_indices = classes.max(dim=1)
+        y = Variable(torch.eye(10)).index_select(dim=0, index=max_length_indices.data)
+        z = x * y[:, :, None]
+
         # regression stream
-        x_r = self.layer4_reg(x)
+        # x_r = self.layer4_reg(x)
         
-        f_r = self.l4_reg_toVec(x_r)
-        f_lin = f_r.view(f_r.size(0), -1) # 1D per batch
+        # f_r = self.l4_reg_toVec(x_r)
+        # f_lin = f_r.view(f_r.size(0), -1) # 1D per batch
                
-        p = self.fc(f_lin)
+        # p = self.fc(f_lin)
+        p = self.fc(z.reshape(32, -1))
+
         #print('f_lin.size()',f_lin.size())
         return {self.output_keys[0]: p} #{'3D': p, '2d_heat': h}
 
