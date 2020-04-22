6d3e151412955ff6733f823a95e83db4b3057d1a
Peiyuan Zhu <30366447+garyzhubc@users.noreply.github.com>
2020-04-22 11:10:37 -0700

    Update config_train_encodeDecode.py

--- project/UnsupervisedGeometryAwareRepresentationLearning/python/configs/config_train_encodeDecode.py
+++ project/UnsupervisedGeometryAwareRepresentationLearning/python/configs/config_train_encodeDecode.py
@@ -6,18 +6,20 @@ inputDimension = 128
 
 config_dict = {
     # general params
-    'dpi' : 190,
+    'dpi': 190,
     'config_class_file': 'dict_configs/config_class_encodeDecode.py',
+
     'input_types'       : ['img_crop','extrinsic_rot','extrinsic_rot_inv','bg_crop'],
     'output_types'      : ['img_crop'], # ['3D','img_crop'],
     'label_types_train' : ['img_crop','3D','bounding_box_cam','intrinsic_crop','extrinsic_rot','extrinsic_rot_inv'],
     'label_types_test'  : ['img_crop','3D','bounding_box_cam','intrinsic_crop','extrinsic_rot','extrinsic_rot_inv'],
     'num_workers'       : 8,
-    
-    # problem class parameters
-    'bones' : skeleton.bones_h36m,
 
-    # opt parameters    
+    # problem class parameters
+    'bones': skeleton.bones_h36m,
+
+
+    # opt parameters
     'num_training_iterations' : 600000,
     'save_every' : 1000, # 100000,
     'learning_rate' : 1e-3,# baseline: 0.001=1e-3
@@ -26,72 +28,77 @@ config_dict = {
     'print_every' : 100,
 
     # network parameters
-    'batch_size_train' : 16,
-    'batch_size_test' : 16, #10 #self.batch_size # Note, needs to be = self.batch_size for multi-view validation
-    'outputDimension_3d' : numJoints * 3,
-    'outputDimension_2d' : inputDimension // 8,
+    'batch_size_train': 16,
+    'batch_size_test': 16,  # 10 #self.batch_size # Note, needs to be = self.batch_size for multi-view validation
+    'outputDimension_3d': numJoints * 3,
+    'outputDimension_2d': inputDimension // 8,
 
-    # loss 
-    'train_scale_normalized' : True,
-    'train_crop_relative' : False,
+    # loss
+    'train_scale_normalized': True,
+    'train_crop_relative': False,
 
     # dataset
     # /bigdata/dingding/cpsc-533/project/UnsupervisedGeometryAwareRepresentationLearning/data/H36M-MultiView-train/
-    'dataset_folder_train' :'/bigdata/dingding/cpsc-533/project/UnsupervisedGeometryAwareRepresentationLearning/data/H36M-MultiView-train',
-    'dataset_folder_test': '/bigdata/dingding/cpsc-533/project/UnsupervisedGeometryAwareRepresentationLearning/data/H36M-MultiView-test',
+    'dataset_folder_train' :'../../../../data.nosync/H36M-MultiView-train',
+    'dataset_folder_test': '../../../../data.nosync/H36M-MultiView-test',
     # 'dataset_folder_train' : '/cvlabdata1/home/rhodin/code/humanposeannotation/python/pytorch_human_reconstruction/TMP/H36M-MultiView-train',
     # 'dataset_folder_test' : '/cvlabdata1/home/rhodin/code/humanposeannotation/python/pytorch_human_reconstruction/TMP/H36M-MultiView-test',
-    #'dataset_folder' :'/Users/rhodin/H36M-MultiView-test',
-    'training_set' : 'h36m',
-    'img_mean' : (0.485, 0.456, 0.406),
-    'img_std' : (0.229, 0.224, 0.225),
-    'actor_subset' : [1,5,6,7,8], # all training subjects
-    'active_cameras' : False,
-    'inputDimension' : inputDimension,
-    'mirror_augmentation' : False,
-    'perspectiveCorrection' : True,
-    'rotation_augmentation' : True,
-    'shear_augmentation' : 0,
-    'scale_augmentation' : False,
-    'seam_scaling' : 1.0,
-    'useCamBatches' : 4,
-    'useSubjectBatches' : True,
-    'every_nth_frame' : 100,
-    
-    'note' : 'resL3',
+    # 'dataset_folder' :'/Users/rhodin/H36M-MultiView-test',
+    'training_set': 'h36m',
+    'img_mean': (0.485, 0.456, 0.406),
+    'img_std': (0.229, 0.224, 0.225),
+    'actor_subset': [1, 5, 6, 7, 8],  # all training subjects
+    'active_cameras': False,
+    'inputDimension': inputDimension,
+    'mirror_augmentation': False,
+    'perspectiveCorrection': True,
+    'rotation_augmentation': True,
+    'shear_augmentation': 0,
+    'scale_augmentation': False,
+    'seam_scaling': 1.0,
+    'useCamBatches': 4,
+    'useSubjectBatches': True,
+    'every_nth_frame': 100,
+
+    'note': 'resL3',
 
     # encode decode
-    'latent_bg' : 0,
-    'latent_fg' : 24,
-    'latent_3d' : 200*3,
-    'latent_dropout' : 0.3,
-    'from_latent_hidden_layers' : 0,
-    'upsampling_bilinear' : 'upper',
-    'shuffle_fg' : True,
-    'shuffle_3d' : True,
-    'feature_scale' : 4,
-    'num_encoding_layers' : 4,
-    'loss_weight_rgb' : 1,
-    'loss_weight_gradient' : 0.01,
-    'loss_weight_imageNet' : 2,
-    'loss_weight_3d' : 0,
-    'do_maxpooling' : False,
-    'encoderType' : 'UNet',
-    'implicit_rotation' : False,
-    'predict_rotation' : False,
-    'skip_background' : True,
+    'latent_bg': 0,
+    'latent_fg': 24,
+    'latent_3d': 200 * 3,
+    'latent_dropout': 0.3,
+    'from_latent_hidden_layers': 0,
+    'upsampling_bilinear': 'upper',
+    'shuffle_fg': True,
+    'shuffle_3d': True,
+    'feature_scale': 4,
+    'num_encoding_layers': 4,
+    'loss_weight_rgb': 1,
+    'loss_weight_gradient': 0.01,
+    'loss_weight_imageNet': 2,
+    'loss_weight_3d': 0,
+    'do_maxpooling': False,
+    'encoderType': 'UNet',
+    'implicit_rotation': False,
+    'predict_rotation': False,
+    'skip_background': True,
+
+    'num_digit_caps':10,
+    'num_caps_out_channel':160,
+    'masked':False
 }
 
 # learning rate influence
 config_dict['learning_rate'] = 1e-3
-config_dict['actor_subset']  = [1,5,6,7,8]
+config_dict['actor_subset'] = [1, 5, 6, 7, 8]
 
 config_dict['batch_size_train'] = 16
 config_dict['batch_size_test'] = 8
 
-config_dict['latent_fg'] = 128; config_dict['feature_scale'] = 2
+config_dict['latent_fg'] = 128;
+config_dict['feature_scale'] = 2
 
-config_dict['loss_weight_rgb']      = 1
+config_dict['loss_weight_rgb'] = 1
 config_dict['loss_weight_gradient'] = 0
 config_dict['loss_weight_imageNet'] = 2
 
@@ -126,6 +133,6 @@ if 1:
 
 # smaller unsupervised subsets
 if 1:
-    config_dict['actor_subset'] = [1,5,6,7,8]
-    #config_dict['actor_subset'] = [1,5,6]
+    config_dict['actor_subset'] = [1, 5, 6, 7, 8]
+    # config_dict['actor_subset'] = [1,5,6]
     config_dict['actor_subset'] = [1]
